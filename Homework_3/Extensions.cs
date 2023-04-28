999f3ed5626014488d36334563773001c19cfd77
tres-bien <denischudinov1212@gmail.com>
2023-04-28 21:10:19 +0300

    Homework_3

---
+++ Homework_3/Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework_3
+{
+    internal static class Extensions
+    {
+
+        public static void DoubleWork<T>(this T value) where T : IPull, IPush
+        {
+            value.Pull();
+            value.Push();
+        }
+    }
+}
---
+++ Homework_3/Train.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3
+{
+    internal class Train : IPush, IPull
+    {
+        public void Pull()
+        {
+            Console.WriteLine("pull-pull"); ;
+        }
+
+        public void Push()
+        {
+            Console.WriteLine("shove-shove");
+        }
+    }
+}
