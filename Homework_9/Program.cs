f793df2ad905b8e371d37c8433077eb3200623a6
tres-bien <denischudinov1212@gmail.com>
2023-06-04 16:48:29 +0300

    Homework_9

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -22,10 +22,15 @@ namespace Homework_9
             Console.WriteLine();
             SaveValue("name", baseClass, "Emma", baseClass.GetType());
             Console.WriteLine(GetAllFields(baseClass));
-            
+
             Console.WriteLine();
             SaveValue("name", baseClass, "Dave");
             Console.WriteLine(GetAllFields(baseClass));
+
+            Console.WriteLine("_________");
+            
+            Console.WriteLine(GetAllFields(SaveValue(GetAllFields(baseClass), baseClass)));
+            SaveValue("name", baseClass, "Eshly");
         }
 
 
@@ -44,9 +49,37 @@ namespace Homework_9
             string str = string.Empty;
             foreach (var item in baseClass.GetType().GetFields())
             {
-                str += $"{item.Name}:{item.GetValue(baseClass)};\n";
+                str += $"{item.Name}:{item.GetValue(baseClass)};";
             }
             return str;
+        }
+
+        static T SaveValue<T>(string prop, T obj)
+        {
+            var a = StringArr(prop);
+            var newType = Activator.CreateInstance(obj.GetType());
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (int.TryParse((a[i][1]), out int value))
+                {
+                    newType.GetType().GetField(a[i][0]).SetValue(obj, int.Parse(a[i][1]));
+                }
+                else
+                newType.GetType().GetField(a[i][0]).SetValue(obj, a[i][1]);
+            }
+            return obj;
+        }
+
+        static List<List<string>> StringArr(string prop)
+        {
+            var list = new List<List<string>>();
+            var arr = prop.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in arr)
+            {
+                list.Add(item.Split(':', StringSplitOptions.RemoveEmptyEntries).ToList());
+            }
+
+            return list;
         }
 
         static object SaveValue(string prop ,object obj ,string val, Type type)
